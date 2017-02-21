7717c17dc59491c9b6a49d92e85e31ef941bd127
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-02-21 01:33:35 +0000

    school controller actions, routes and basic new and edit views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :schools do
+    member do
+      get 'activate'
+    end
+  end
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
